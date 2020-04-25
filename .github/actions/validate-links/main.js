e05bfafeafbfb911ea36999b5e7a62fa2ba2f321
Ira Lun <sammyrosajoe@gmail.com>
2020-04-25 05:44:01 +0100

    Fix action by using JS async.

--- .github/actions/validate-links/main.js
+++ .github/actions/validate-links/main.js
@@ -1,61 +1,68 @@
 const core = require('@actions/core');
-const fs = require('fs');
+const fs = require('fs/promises');
 const http = require('http');
 const https = require('https');
 
 
-function isValidLink(link) {
+// TODO: Limit for acceptable delay in response?
+// TODO: Try HEAD first, then if not available, GET?
+
+
+async function checkProgramLink(program) {
 	try {
-		console.log("link: " + link);
 		var url = new URL(link);
 	} catch (error) {
 		// url is invalid
-		console.log("Invalid URL! " + link);
+		console.log(`Program "${program.program_name}", policy_url ${program.policy_url}: Invalid URL.\n`);
 		return false;
 	}
+
 	if (url.protocol === 'https:') {
 		var protocol = https;
 	} else if (url.protocol === 'http:') {
 		var protocol = http;
 	} else {
-		console.log("Invalid protocol.");
+		console.log(`Program "${program.program_name}", policy_url ${program.policy_url}: URL protocol not HTTPS or HTTP.\n`);
 		return false;
 	}
 	
-	protocol.get(url, (response) => {
-		console.log("Status code check.");
-		response.resume();
-		return response.statusCode === 200;
-	}).on('error', (error) => {
-		console.log(".get() error.");
-		return false;
+	let responsePromise = new Promise((resolve) => {
+		protocol.get(url, (response) => {
+			if (response.statusCode === 200) {
+				resolve(true);
+			} else {
+				console.log(`Program "${program.program_name}", policy_url ${program.policy_url}: Responded with ${response.statusCode} ${response.statusMessage}.\n`);
+				resolve(false);
+			};
+		}).on('error', (error) => {
+			console.log(`Program "${program.program_name}", policy_url ${program.policy_url}: ${error.message}\n`);
+			resolve(false);
+		}).resume();
 	});
 }
 
-function programHasInvalidURL(program) {
-	var result = isValidLink(program['policy_url']);
-	console.log("undefined? " + result === undefined);
-	console.log("null? " + result === null);
-	console.log(result + program['policy_url']);
-	return !result;
-}
 
-try {
-	console.log('Validating links...');
-	fs.readFile('./program-list/program-list.json', (error, data) => {
-		if (error) throw error;
-		let json = JSON.parse(data);
-		let programsWithBrokenLinks = json.filter(programHasInvalidURL);
-		if (programsWithBrokenLinks) {
-			let errorMessage = "";
-			programsWithBrokenLinks.forEach(function(program) {
-				errorMessage += 'Program ' + program['program_name'] + ' has invalid policy_url ' + program["policy_url"] + '.\n';
-			});
-			throw new Error(errorMessage);
-		} else {
-			console.log('Links appear to be valid.');
-		}
+(async function main() {
+	console.log('Validating program links...');
+
+	try {
+		var file = await fs.readFile('./program-list/program-list.json', "UTF-8");
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+
+	let programsList = JSON.parse(file);
+
+	let promises = [];
+	programsList.forEach((program) => {
+		promises.push(await checkProgramLink(program));
 	});
-} catch (error) {
-	core.setFailed(error.message);
-}
+	Promise.allSettled(promises).then(results => {
+		results.forEach(result => {
+			if (result.value === false) {
+				core.setFailed('Invalid program link(s) found.');
+				// TODO: Action should terminate here?
+			}
+		});
+	});
+})();
