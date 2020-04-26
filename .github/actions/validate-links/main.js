f212987f7c558148dacbfb1794b23aab4d551ce2
Ira Lun <sammyrosajoe@gmail.com>
2020-04-26 14:47:30 +0100

    Rewrite to try more synchronised approach.
    We wait for the result from one program to return before checking the
    next.

--- .github/actions/validate-links/main.js
+++ .github/actions/validate-links/main.js
@@ -1,14 +1,14 @@
 const core = require('@actions/core');
-const fs = require('fs').promises;
+const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
 
-// TODO: Limit for acceptable delay in response?
+// TODO: response timeout limit?
 // TODO: Try HEAD first, then if not available, GET?
 
 
-async function checkProgramLink(program) {
+const checkProgramLink = async (program) => {
 	try {
 		var url = new URL(program.policy_url);
 	} catch (error) {
@@ -43,28 +43,24 @@ async function checkProgramLink(program) {
 }
 
 
-(async function main() {
+(async () => {
 	console.log('Validating program links...');
 
 	try {
-		var file = await fs.readFile('./program-list/program-list.json', "UTF-8");
+		var file = fs.readFileSync('./program-list/program-list.json', 'UTF-8');
 	} catch (error) {
 		core.setFailed(error.message);
 	}
 
 	let programsList = JSON.parse(file);
 
-	let promises = [];
-	programsList.forEach(async (program) => {
-		var promise = await checkProgramLink(program);
-		promises.push(promise);
+	let results = [];
+	programsList.forEach((program) => {
+		let result = await checkProgramLink(program);
+		results.push(result);
 	});
-	Promise.allSettled(promises).then(results => {
-		results.forEach(result => {
-			if (result.value === false) {
-				core.setFailed('Invalid program link(s) found.');
-				// TODO: Action should terminate here?
-			}
-		});
-	});
-})();
+
+	if (results.includes(false)) {
+		core.setFailed('Invalid program link(s) found.');
+	}
+})()
