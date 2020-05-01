e206871482aef68a9a3a9ab76dfa7cad38a6aadd
Ira Lun <sammyrosajoe@gmail.com>
2020-05-01 21:07:37 +0100

    Return final promise in attempt to get Node to wait for it.

--- .github/actions/validate-links/main.js
+++ .github/actions/validate-links/main.js
@@ -89,7 +89,7 @@ async function checkProgramLink(program, programId) {
 		promises.push(promise);
 	});
 	console.log('All promises pushed.');
-	await Promise.allSettled(promises).then(results => {
+	const finalPromise = await Promise.allSettled(promises).then(results => {
 		results.forEach(result => {
 			console.log(`result.value = ${result.value}.`);
 			if (result.value === false) {
@@ -98,4 +98,5 @@ async function checkProgramLink(program, programId) {
 			}
 		});
 	});
+	return finalPromise;
 })();
