b9660b7848551aa927519677e605e1a8c3aae477
Ira Lun <sammyrosajoe@gmail.com>
2020-05-03 19:54:55 +0100

    Try removing Connection: close header.

--- .github/actions/validate-links/main.js
+++ .github/actions/validate-links/main.js
@@ -45,7 +45,7 @@ const checkPolicyURL = async (program) => (
 		}
 	
 		var request = protocol.get(url, {'headers': {
-			'Connection': 'close',
+			//'Connection': 'close',
 			'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Mobile Safari/537.36',
 			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 			'accept-encoding': 'gzip, deflate, br',
