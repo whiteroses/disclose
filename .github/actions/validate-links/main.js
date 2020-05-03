5872e6d72c7431f527bf0ccd4de6f75697655ff0
Ira Lun <sammyrosajoe@gmail.com>
2020-05-03 11:37:20 +0100

    More refactoring.

--- .github/actions/validate-links/main.js
+++ .github/actions/validate-links/main.js
@@ -17,22 +17,22 @@ const logEnded = function(programId) {
 */
 const REQUEST_TIMEOUT = 5000;
 
-async function checkPolicyURL(program, programId) {
-	try {
-		var url = new URL(program.policy_url);
-	} catch (error) {
-		return [programId, 'Not a URL.'];
-	}
+const checkPolicyURL = async (program) => (
+	new Promise((resolve, reject) => {
+		try {
+			var url = new URL(program.policy_url);
+		} catch (error) {
+			reject('Not a URL.');
+		}
 
-	if (url.protocol === 'https:') {
-		var protocol = https;
-	} else if (url.protocol === 'http:') {
-		var protocol = http;
-	} else {
-		return [programId, 'URL protocol not HTTPS or HTTP.'];
-	}
+		if (url.protocol === 'https:') {
+			var protocol = https;
+		} else if (url.protocol === 'http:') {
+			var protocol = http;
+		} else {
+			reject('URL protocol not HTTPS or HTTP.');
+		}
 	
-	return new Promise((resolve) => {
 		var request = protocol.get(url, {'headers': {'Connection': 'close'}}, response => {
 			response.on('end', () => {
 				// TODO: Is this necessary?
@@ -41,19 +41,19 @@ async function checkPolicyURL(program, programId) {
 			if (response.statusCode === 200) {
 				resolve(true);
 			} else {
-				resolve([programId, `Responded with ${response.statusCode} ${response.statusMessage}.`]);
+				reject(`Responded with ${response.statusCode} ${response.statusMessage}.`);
 			};
 		}).on('aborted', (error) => {
-			resolve([programId, error.toString()]);
+			reject(error.toString());
 		}).on('error', (error) => {
-			resolve([programId, error.toString()]);
+			reject(error.toString());
 		});
 		request.setTimeout(REQUEST_TIMEOUT, function() {
 			request.abort();
-			resolve([programId, `Request ${programId} timed out.`]);
+			reject(`Request ${programId} timed out.`);
 		});
-	});
-}
+	})
+);
 
 
 var done = false;
@@ -74,14 +74,16 @@ var done = false;
 	var checks = programsList.map(checkPolicyURL);
 
 	Promise.allSettled(checks).then(results => {
-		var invalidPrograms = results.filter(result => (result !== true));
-		if (invalidPrograms.length) {
-			for (const invalidProgram of invalidPrograms) {
-				let [programId, message] = invalidProgram;
+		let invalidURLsCount = 0;
+		for (const [programId, result] of results.entries()) {
+			if (result.status === 'rejected') {
+				++invalidURLFound;
 				let program = programsList[programId];
-				console.log(`Program "${programId + 1}. ${program.program_name}" (policy_url ${program.policy_url}): ${message}.`);
+				console.log(`"${programId + 1}. ${program.program_name}" (policy_url ${program.policy_url}): ${result.reason}.`);
 			}
-			core.setFailed('Invalid policy URL(s) found.');
+		}
+		if (invalidURLsCount) {
+			core.setFailed(`${invalidURLsCount} invalid policy URL${invalidURLsCount === 1? '': 's'} found.`);
 		} else {
 			console.log('All policy URLs appear to be valid.');
 		}
