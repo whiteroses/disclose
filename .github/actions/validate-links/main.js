21101e6f30bff93d80d42d134462a672c94aa8a3
Ira Lun <sammyrosajoe@gmail.com>
2020-05-11 01:03:53 +0100

    Try commenting out options.

--- .github/actions/validate-links/main.js
+++ .github/actions/validate-links/main.js
@@ -75,26 +75,26 @@ const checkPolicyURL = (program) => new Promise(
     let incomingMessage;
     try {
       incomingMessage = await got(url, {
-        'headers': {
-          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,' +
-            'image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;' +
-            'q=0.9',
-          'accept-encoding': 'gzip, deflate, br',
-          'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8',
-          'connection': 'close',
-          'cache-control': 'max-age=0',
-          'sec-fetch-dest': 'document',
-          'sec-fetch-mode': 'navigate',
-          'sec-fetch-site': 'none',
-          'sec-fetch-user': '?1',
-          'upgrade-insecure-requests': '1',
-          'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' +
-            '(KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
-        },
-        'followRedirect': false,  // TODO: Does it follow 303 redirects regardless?
-        'retry': 0,
-        'throwHttpErrors': false, // TODO: non-2xx status codes are resolved instead?
-        'timeout': TIMEOUT,
+        //'headers': {
+        //  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,' +
+        //    'image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;' +
+        //    'q=0.9',
+        //  'accept-encoding': 'gzip, deflate, br',
+        //  'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8',
+        //  'connection': 'close',
+        //  'cache-control': 'max-age=0',
+        //  'sec-fetch-dest': 'document',
+        //  'sec-fetch-mode': 'navigate',
+        //  'sec-fetch-site': 'none',
+        //  'sec-fetch-user': '?1',
+        //  'upgrade-insecure-requests': '1',
+        //  'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' +
+        //    '(KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
+        //},
+        //'followRedirect': false, // TODO: Does it follow 303 redirects regardless?
+        //'retry': 0,
+        //'throwHttpErrors': false, // TODO: non-2xx status codes are resolved instead?
+        //'timeout': TIMEOUT,
       });
     } catch (error) {
       resolve(error.toString());
